5deb9e074202cd786d82923a3a288ee8bfa41510
KamranMdShaikh <kamranmdshaikh@gmail.com>
2026-03-03 23:45:21 +0600

    streamlit v-01

---
+++ eq_dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+st.set_page_config(
+    
+    page_title = "Earthquake Decision Support"
+    layout = 'wide'
+    initial_sidebar_state='expanded'
+)
